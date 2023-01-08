7eae9b4b5b4246d1df96908a7af1421259c5c1ed
jie.wu <wujie0628@126.com>
2023-01-08 15:40:19 +0800

    tabbar正常运行

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+
+    <div :style="colorStyle">
+      <slot name="item-text"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    data() {
+      return {
+        // isActive: false
+      }
+    },
+    props: {
+      path: {
+        type: String,
+        default: '/home'
+      },
+      activeColor: {
+        type: String,
+        default: 'blue'
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path === this.path
+      },
+      colorStyle() {
+        return this.isActive ? {color : this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.push(this.path)
+      }
+    },
+  }
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+  }
+
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+      resolve: {
+        alias: {
+          'assets': '@/assets',
+          'components': '@/components',
+          'views': '@/views',
+          'common': '@/common',
+          'network': '@/network'
+        }
+      }
+    },
 })
