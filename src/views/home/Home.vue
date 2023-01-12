9148390e52a9e4a1a4c2334de51af21d75856273
jie.wu <wujie0628@126.com>
2023-01-12 22:31:41 +0800

    tabControl的吸顶效果完成。完成的原理是新建一个tab，默认隐藏，当滑动到对应位置时让其显示出来

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,21 +4,18 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <scroll class="content" 
-            ref="scroll" 
-            :probeType="3" 
-            @scroll="scroll" 
-            :pullUpLoad="true"
-            @pullingUp="loadMore">
-      <home-swiper :banners="banners"></home-swiper>
+    <tab-control :tabs="['流行', '新款', '精选']" @tabClick="tabClick" ref="tabControl1" class="tab-control"
+      v-show="isTabFixed"></tab-control>
+    <scroll class="content" ref="scroll" :probeType="3" @scroll="scroll" :pullUpLoad="true" @pullingUp="loadMore">
+      <home-swiper :banners="banners" @swiperImgLoad="swiperImgLoad"></home-swiper>
       <home-recommend :recommends="recommends"></home-recommend>
       <popular></popular>
-      <tab-control :tabs="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control class="tab-control">
+      <tab-control :tabs="['流行', '新款', '精选']" @tabClick="tabClick" ref="tabControl2"></tab-control>
       <goods-list :goodsList="goods[currentType].list"></goods-list>
     </scroll>
-    
-    <back-top @click.native="toTop" v-show="isShowBackTop"/>
-    
+
+    <back-top @click.native="toTop" v-show="isShowBackTop" />
+
   </div>
 
 </template>
@@ -38,6 +35,9 @@
     getHomeData
   } from "network/home"
 
+  import {
+    debounce
+  } from "common/utils"
   export default {
     components: {
       NavBar,
@@ -68,7 +68,9 @@
           },
         },
         currentType: "pop",
-        isShowBackTop: false
+        isShowBackTop: false,
+        tabOffsetTop: 0,
+        isTabFixed: false
       }
     },
     created() {
@@ -81,12 +83,16 @@
       this.getHomeData('sell')
     },
     mounted() {
+      const refresh = debounce(this.$refs.scroll.refresh, 100)
+
       //监听列表图片加载完成事件
-      this.$bus.$on("goodsItemImgLoad", ()=>{
-        this.$refs.scroll.refresh();
+      this.$bus.$on("goodsItemImgLoad", () => {
+        refresh()
       })
+
     },
     methods: {
+
       /**
        * 事件点击相关
        */
@@ -101,18 +107,29 @@
           case 2:
             this.currentType = "sell";
             break;
-        }
+        };
+        this.$refs.tabControl1.currentIndex = index;
+        this.$refs.tabControl2.currentIndex = index;
       },
       toTop() {
         this.$refs.scroll.toTop(300);
       },
-      
+
       scroll(position) {
+        //是否显示向上小箭头
         this.isShowBackTop = (-position.y) > 1000
+
+        //tab是否吸顶
+        this.isTabFixed = (-position.y) > this.tabOffsetTop
       },
-      
+
       loadMore() {
         this.getHomeData(this.currentType);
+      },
+      swiperImgLoad() {
+        //获取tab-control的fixed高度
+        this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
+
       },
       /**
        * 网络相关
@@ -128,7 +145,7 @@
         getHomeData(type, reqPage).then(res => {
           this.goods[type].list.push(...res.data.list)
           this.goods[type].page += 1
-          
+
           this.$refs.scroll.finishPullUp()
         })
       }
@@ -138,7 +155,7 @@
 
 <style scoped>
   #home {
-    padding-top: 44px; 
+    /* padding-top: 44px; */
     height: 100vh;
     position: relative;
   }
@@ -147,17 +164,11 @@
     background-color: var(--color-tint);
     color: #fff;
 
-    position: fixed;
+    /* position: fixed;
     left: 0;
     right: 0;
     top: 0;
-    z-index: 9;
-  }
-
-  .tab-control {
-    position: sticky;
-    top: 44px;
-    z-index: 9;
+    z-index: 9; */
   }
 
   .content {
@@ -166,9 +177,13 @@
     position: absolute;
     top: 44px;
     bottom: 49px;
-    left: 0;   
+    left: 0;
     right: 0;
 
   }
-</style>  
- 
+
+  .tab-control {
+    position: relative;
+    z-index: 9;
+  }
+</style>
--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -3,7 +3,7 @@
   <swiper>
     <swiper-item v-for="item in banners">
       <a :href="item.link">
-        <img :src="item.image" alt="">
+        <img :src="item.image" alt="" @load="imgLoad">
       </a>
     </swiper-item>
   </swiper>
@@ -18,11 +18,25 @@
       Swiper,
       SwiperItem
     },
+    data() {
+      return {
+        isImgLoad: false
+      }
+    },
     props: {
       banners: {
         type: Array,
         default: []
       },
+    },
+    methods: {
+      imgLoad() {
+        if(!this.isImgLoad) {
+          this.$emit("swiperImgLoad")
+          this.isImgLoad = true;
+        }
+        
+      }
     },
   }
   
