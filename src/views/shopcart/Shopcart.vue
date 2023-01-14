f243623c1a467521614e8c447ef5ffd7e21ca224
jie.wu <wujie0628@126.com>
2023-01-15 00:38:46 +0800

    购物车列表滚动完成，页面待完成

--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -1,9 +1,38 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{cartLength}})</div>
+    </nav-bar>
+    
+    <cart-list></cart-list>
+  </div>
+  
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar"
+  import CartList from "./childComp/CartList"
+  
+  import {mapGetters} from "vuex"
+  
+  export default {  
+    name: "Shopcart",
+    components: {
+      NavBar,
+      CartList
+    },
+    computed: {
+      ...mapGetters(['cartLength'])
+    },
+  }
 </script>
 
-<style>
+<style scoped>
+  .cart {
+    height: 100vh;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
---
+++ src/views/shopcart/childComp/CartListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    {{product}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        default: {}
+      },
+    },
+  }
+</script>
+
+<style>
+</style>
